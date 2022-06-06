6afc5fd898a8053ba3d032678ebe0a8a93d1ccc1
Mateusz <milczak.mateusz@gmail.com>
2022-06-06 20:00:56 +0200

    Fixed postbuild copy and cleaned repo

--- Engine/src/Engine/Core.h
+++ Engine/src/Engine/Core.h
@@ -16,8 +16,8 @@
 #endif
 
 #ifdef LOG_ENABLE_ASSERTS
-	#define LOG_ASSERT(x, ...) { if(!(x)) { LOG_ERROR("Assertion Failed: {0}", __VA_ARGS__)}; __debugbreak(); } }
-	#define LOG_CORE_ASSERT(x, ...) { if(!(x)) { LOG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__)}; __debugbreak(); } }
+	#define LOG_ASSERT(x, ...) { if(!(x)) { LOG_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define LOG_CORE_ASSERT(x, ...) { if(!(x)) { LOG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
 	#define LOG_ASSERT(x, ...)
 	#define LOG_CORE_ASSERT(x, ...)
